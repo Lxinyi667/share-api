c85bcd4664a9ea2cc6f51f7777a8a2f5d9f32045
Lxinyi667 <1716186890@qq.com>
2023-10-07 16:53:03 +0800

    使用雪花算法生成数据库 ID

--- share-api/share-user/src/main/java/top/lxyi/share/user/service/UserService.java
+++ share-api/share-user/src/main/java/top/lxyi/share/user/service/UserService.java
@@ -5,9 +5,12 @@ import jakarta.annotation.Resource;
 import org.springframework.stereotype.Service;
 import top.lxyi.share.common.exception.BusinessException;
 import top.lxyi.share.common.exception.BusinessExceptionEnum;
+import top.lxyi.share.common.util.SnowUtil;
 import top.lxyi.share.user.domain.dto.LoginDTO;
 import top.lxyi.share.user.domain.entity.User;
 import top.lxyi.share.user.mapper.UserMapper;
+
+import java.util.Date;
 
 @Service
 public class UserService {
@@ -28,5 +31,29 @@ public class UserService {
         throw new BusinessException(BusinessExceptionEnum.PASSWORD_ERROR);
         }
         return userDB;
+    }
+    public Long register(LoginDTO loginDTO){
+//        根据手机号查询用户
+        User userDB= userMapper.selectOne(new QueryWrapper<User>().lambda().eq(User::getPhone,loginDTO.getPhone()));
+        //        找到了手机号已经被注册
+        if (userDB != null){
+            throw new BusinessException(BusinessExceptionEnum.PHONE_EXIST);
+        }
+        User savedUser = User.builder()
+                //        使用雪花算法生成id
+                .id(SnowUtil.getSnowflakeNextId())
+                .phone(loginDTO.getPhone())
+                .password(loginDTO.getPassword())
+                .nickname("新用户")
+                .roles("user")
+                .avatarUrl("https://i2.100024.xyz/2023/01/26/3exzjl.webp")
+                .bonus(100)
+                .createTime(new Date())
+                .updateTime(new Date())
+                .build();
+        userMapper.insert(savedUser);
+        return savedUser.getId();
+
+
     }
 }
